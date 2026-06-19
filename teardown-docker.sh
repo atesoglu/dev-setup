984f03ecb9c954a68d4ac8a06c5d50b59738b4cb
Kadir Atesoglu <atesoglu@live.com>
2026-06-19 13:02:42 +0200

    Minor improvements and small fixes

--- teardown-docker.sh
+++ teardown-docker.sh
@@ -3,6 +3,6 @@
 echo "🧹 Stopping containers and removing volumes..."
 
 # Fixed paths and updated to modern 'docker compose' syntax
-docker compose -f elk/docker-compose.yml -f database/database/docker-compose.yml -p "dev-resources" down -v --remove-orphans
+docker compose -f elk/docker-compose.yml -f database/docker-compose.yml -p "dev-resources" down -v --remove-orphans
 
 echo "✅ Teardown complete."
